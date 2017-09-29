34e4f536c2fa78227558fb20c65cef5de018d6e9
rockstarrevathi <jannavarapu.revathi@gmail.com>
2017-09-29 15:00:53 -0500

    final commit

---
+++ app/src/main/java/com/example/android/habittracker/HabitContract.java
@@ -0,0 +1,25 @@
+package com.example.android.habittracker;
+
+import android.provider.BaseColumns;
+
+
+public class HabitContract {
+
+    public HabitContract() {
+    }
+
+    public class HabitEntry implements BaseColumns {
+
+        public final static String TABLE_NAME = "tracking_diary";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_DATE = "date";
+        public final static String COLUMN_HABIT = "habit";
+        public final static String COLUMN_COMMENT = "comment";
+
+        public final static int HABIT_UDACITY = 0;
+        public final static int HABIT_PROGRAMMING = 1;
+        public final static int HABIT_WORKOUT = 2;
+    }
+
+}
